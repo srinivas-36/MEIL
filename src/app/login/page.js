01ef7475d06cd00f3605b88ae982f301e96d6824
srinivas-36 <126503015+srinivas-36@users.noreply.github.com>
2025-09-03 21:21:42 +0530

    Update page.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -198,7 +198,7 @@ export default function Login() {
 
                         <div className="text-center">
                             <p className="text-sm text-gray-600">
-                                Don't have an account.?
+                                Dont have an account?
                                 <Link href="/signup" className="font-medium text-blue-600 hover:text-blue-500">
                                     Sign up here
                                 </Link>
