498fe185fd94a9e596a4fba680e00d19af21f1b9
srinivas-36 <126503015+srinivas-36@users.noreply.github.com>
2025-09-03 21:45:51 +0530

    Update page.js

--- src/app/(main)/app/page.js
+++ src/app/(main)/app/page.js
@@ -264,7 +264,7 @@ export default function Home() {
                   ) : (
                     <div>
                       <h3 className="font-medium text-red-700 mb-2">Material Not Found</h3>
-                      <p className="text-gray-700 mb-3">No material found with number "{searchByNumber}"</p>
+                      <p className="text-gray-700 mb-3">No material found with number {searchByNumber}</p>
                       <div className="flex space-x-2">
                         <button
                           onClick={() => {
